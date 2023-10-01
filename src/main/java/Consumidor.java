3c8700a132d2ef0b6f4c4fbef1d96c1bec2afc97
Felipe Targino <targino.felipe@academico.ifpb.edu.br>
2023-10-01 11:39:09 -0300

    rabbit-basico

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -5,25 +5,45 @@ import com.rabbitmq.client.DeliverCallback;
 
 
 public class Consumidor {
-    public static void main(String[] args) throws Exception {
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost("localhost");
-        Connection conexao = connectionFactory.newConnection();
-        Channel canal = conexao.createChannel();
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
-        String NOME_FILA = "plica"
-                + "";
-        canal.queueDeclare(NOME_FILA, false, false, false, null);
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        DeliverCallback callback = (consumerTag, delivery) -> {
-            String mensagem = new String(delivery.getBody());
-            System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println("[*] Aguardando mensagens. Para sair, pressione CTRL + C");
+
+        channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String mensagem = new String(delivery.getBody(), "UTF-8");
+
+            System.out.println("[x] Recebido '" + mensagem + "'");
+            try {
+                doWork(mensagem);
+            } finally {
+                System.out.println("[x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
 
-        // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
-        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
-            System.out.println("Cancelaram a fila: " + NOME_FILA);
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {
         });
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
 
--- src/main/java/Produtor.java
+++ src/main/java/Produtor.java
@@ -1,26 +1,26 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class Produtor {
 
-    public static void main(String[] args) throws Exception {
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        try (
-                Connection connection = connectionFactory.newConnection();
-                Channel canal = connection.createChannel();
-        ) {
-            String mensagem = "Olá";
-            String NOME_FILA = "plica";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
-            //(queue, passive, durable, exclusive, autoDelete, arguments)
-            canal.queueDeclare(NOME_FILA, false, false, false, null);
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
 
-            // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
-            canal.basicPublish("", NOME_FILA, false, false, null, mensagem.getBytes());
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
 
+            String mensagem = "Olá, Felipe Targino do Nascimento " + String.join("", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem.getBytes("UTF-8"));
+            System.out.println("[x] Enviado '" + mensagem + "'");
         }
     }
 }
+
 
 
